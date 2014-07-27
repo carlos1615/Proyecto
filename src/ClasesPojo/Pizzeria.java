0af585e4cc8e009c018e4d5269ef0ec22ff9c7a6
Gabriel Acuña <gstef09@hotmail.com>
2014-07-27 15:21:17 -0500

    Clases Pojos actualizada

---
+++ src/ClasesPojo/Pizzeria.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ClasesPojo;
+
+/**
+ *
+ * @author User
+ */
+public class Pizzeria {
+    
+    private int codigoPizzeria;
+    private String Nombre;
+    private int rucPizzeria;
+    private String Representante;
+    
+    private String estadoPizzeria;
+
+    /**
+     * Get the value of estadoPizzeria
+     *
+     * @return the value of estadoPizzeria
+     */
+    public String getEstadoPizzeria() {
+        return estadoPizzeria;
+    }
+
+    /**
+     * Set the value of estadoPizzeria
+     *
+     * @param estadoPizzeria new value of estadoPizzeria
+     */
+    public void setEstadoPizzeria(String estadoPizzeria) {
+        this.estadoPizzeria = estadoPizzeria;
+    }
+
+
+    /**
+     * Get the value of Representante
+     *
+     * @return the value of Representante
+     */
+    public String getRepresentante() {
+        return Representante;
+    }
+
+    /**
+     * Set the value of Representante
+     *
+     * @param Representante new value of Representante
+     */
+    public void setRepresentante(String Representante) {
+        this.Representante = Representante;
+    }
+
+
+    /**
+     * Get the value of rucPizzeria
+     *
+     * @return the value of rucPizzeria
+     */
+    public int getRucPizzeria() {
+        return rucPizzeria;
+    }
+
+    /**
+     * Set the value of rucPizzeria
+     *
+     * @param rucPizzeria new value of rucPizzeria
+     */
+    public void setRucPizzeria(int rucPizzeria) {
+        this.rucPizzeria = rucPizzeria;
+    }
+
+
+    /**
+     * Get the value of Nombre
+     *
+     * @return the value of Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * Set the value of Nombre
+     *
+     * @param Nombre new value of Nombre
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+
+    /**
+     * Get the value of codigoPizzeria
+     *
+     * @return the value of codigoPizzeria
+     */
+    public int getCodigoPizzeria() {
+        return codigoPizzeria;
+    }
+
+    /**
+     * Set the value of codigoPizzeria
+     *
+     * @param codigoPizzeria new value of codigoPizzeria
+     */
+    public void setCodigoPizzeria(int codigoPizzeria) {
+        this.codigoPizzeria = codigoPizzeria;
+    }
+
+}
